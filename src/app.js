3a8a0a862182085e4b6b4f3bbca6ed722809ab6e
MarceloMosl <marcelomosl@icloud.com>
2023-01-12 17:16:31 -0300

    Requisito: Conexão com MongoBD

--- src/app.js
+++ src/app.js
@@ -1,9 +1,44 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
-import { json } from "express/lib/response";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect().then(() => {
+  db = mongoClient.db("projeto14"); //O padrão é test
+});
 
 const server = express();
 server.use(cors());
-server.use(json);
+server.use(json());
+
+server.post("/participants", async (req, res) => {
+  const { name } = req.body;
+  if (!name)
+    return res.sendStatus(422).send(alert("name deve ser string não vazio"));
+
+  const exist = await db.collection("participants").findOne({ name });
+
+  if (exist) return res.sendStatus(409).send(alert("usuario em uso"));
+
+  try {
+    db.collection("participants").insertOne({ name });
+    res.send("usuario Cadastrado");
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+server.get("/participants", (req, res) => {
+  db.collection("participants")
+    .find()
+    .toArray()
+    .then((users) => {
+      res.send(users);
+    });
+});
 
 server.listen(5000, () => console.log("Tst"));
