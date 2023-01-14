005d9b1d26efd009d22a7cf093ab5f210c742a61
MarceloMosl <marcelomosl@icloud.com>
2023-01-14 16:35:02 -0300

    Requisito: Post Msgs

--- src/app.js
+++ src/app.js
@@ -67,10 +67,46 @@ app.get("/messages", async (req, res) => {
   const limit = parseInt(req.query.limit);
   try {
     const messages = await db.collection("messages").find().toArray();
-    if (!limit) return res.send(messages.reverse());
-    res.send(messages.slice(-limit).reverse());
+    if (!limit) return res.send(messages);
+    res.send(messages.slice(-limit));
   } catch (error) {
     res.status(500).send(error);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const user = req.headers.user;
+
+  const schemaMsg = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid("message", "private_message").required(),
+  });
+
+  const validate = schemaMsg.validate(req.body);
+
+  if (validate.error) {
+    const errors = validate.error.details.map((detail) => detail.message);
+    return res.status(422);
+  }
+
+  try {
+    const userExists = await db
+      .collection("participants")
+      .findOne({ name: user });
+    if (!userExists) return res.status(422);
+
+    db.collection("messages").insertOne({
+      to,
+      text,
+      type,
+      from: user,
+      time: dayjs().format("HH:mm:ss"),
+    });
+    res.status(201);
+  } catch (error) {
+    return res.status(500).send(error);
   }
 });
 
